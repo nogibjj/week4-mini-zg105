da10fc8bf391e415182301d8c76f5b47d403192b
Jason-Guo1999 <106792458+Jason-Guo1999@users.noreply.github.com>
2023-09-13 20:57:52 +0000

    new commit

--- generate.py
+++ generate.py
@@ -9,14 +9,14 @@ def generate_grades():
     data = []
     for student_id in range(1, num_students + 1):
         student_name = f"Student {student_id}"
-        grade = random_int = random.randint(50, 100)
+        grade= random.randint(50, 100)
         data.append([student_id, student_name, grade])
 
     # Define the CSV file path
     csv_file_path = "class_grades.csv"
 
     # Write the data to the CSV file
-    with open(csv_file_path, mode="w", newline="") as file:
+    with open(csv_file_path, mode="w", newline="", encoding="UTF-8") as file:
         writer = csv.writer(file)
         writer.writerow(["Student ID", "Student Name", "Grade"])  # Write header row
         writer.writerows(data)  # Write data rows
